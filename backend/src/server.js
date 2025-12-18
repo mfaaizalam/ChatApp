9a796cb35f4663a479d8f389646114fa5b542e17
mfaaizalam <mfaaiz2000bce@gmail.com>
2025-12-19 00:53:12 +0500

    Initial commit with local files

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv  from "dotenv";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT
+
+app.get("/api/auth/signup",(req,res)=>{
+    res.send("signup");
+})
+app.get("/api/auth/login",(req,res)=>{
+    res.send("login");
+})
+app.get("/api/auth/logout",(req,res)=>{
+    res.send("logout");
+})
+app.listen(PORT,()=>{
+    console.log(`Server is running on PORT ${PORT}`);
+})
