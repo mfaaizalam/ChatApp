1a586ef8d9dc97fe76c16268b7d1e85b6f08a53a
mfaaizalam <mfaaiz2000bce@gmail.com>
2025-12-21 23:03:25 +0500

    onboarding,friend request comp

---
+++ backend/src/routes/user.routes.js
@@ -0,0 +1,14 @@
+import express from "express"
+import { protectRoute } from "../middleware/auth.middleware";
+import router from "./auth.route"
+
+const router = express.Router()
+
+// apply auth middleware to all routes
+router.use(protectRoute);
+router.get("/",getRecommendedUsers);
+
+router.get("/friends",getMyfriends);
+
+router.post("/friend-request/:id",sendFriendRequest);
+export default  router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv  from "dotenv";
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/user.routes.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use("/api/auth",authRoutes)
+app.use("/api/user",userRoutes)
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`);
     connectDB();
