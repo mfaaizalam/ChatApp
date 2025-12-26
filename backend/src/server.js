4178854045673cbf2f219fb112896120ce3026a5
mfaaizalam <mfaaiz2000bce@gmail.com>
2025-12-27 01:00:16 +0500

    frontend setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,9 +5,14 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.routes.js";
 import chatRoutes from "./routes/chat.routes.js"
+import cors from "cors";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT
+app.use(cors({
+    origin:"http://localhost:5173", //allow frontend to send cookies
+    credentials:true,
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+import React from 'react'
+import {Route, Routes } from 'react-router'
+import HomePage from "./pages/HomePage.jsx"
+import NotificationPage from "./pages/NotificationPage.jsx"
+import LoginPage from "./pages/LoginPage.jsx"
+import SignUpPage from "./pages/SignUpPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx"
+import CallPage from "./pages/CallPage.jsx"
+import ChatPage from "./pages/ChatPage.jsx"
+import toast, { Toaster } from 'react-hot-toast';
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { axiosInstance } from './lib/axios.js';
+const App = () => {
+  const {data,isLoading,error} = useQuery({ queryKey:["todos"],
+    queryFn:async ()=>{
+      const res =await axiosInstance.get("/api/auth/me");
+      return res.data;
+    },
+    retry:false
+  });
+  console.log(data)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='h-screen' data-theme="coffee">
+      <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/signup" element={<SignUpPage/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/notification" element={<NotificationPage/>}/>
+          <Route path="/call" element={<CallPage/>}/>
+          <Route path="/chat" element={<ChatPage/>}/>
+          <Route path="/onboarding" element={<OnboardingPage/>}/>
+      </Routes>
+      <Toaster/>
+    </div>
   )
 }
 
 export default App
+
+
+
+// useQuery → read data from server
+// useMutation → change server data
+// queryKey → cache name
+// invalidateQueries → reload data
+
+
+
+// READ → useQuery
+// CREATE → useMutation
+// UPDATE → useMutation
+// DELETE → useMutation
+// REFRESH → invalidateQueries
+
+
+// useQuery = fetch/read data
+// useMutation = create/update/delete/change data
+// queryKey = unique name/ID for your data
+// invalidateQueries(queryKey) = refresh/fetch fresh data
+// refetch = manually refetch data
+// isLoading = data is loading
+// isError = error happened
+// data = the fetched data
+
+
+// Axios → sends/gets data from server (HTTP requests).
+// TanStack Query → manages your data in React (caching, updating, syncing).
+// Together → Axios fetches or changes data, TanStack Query stores it, refreshes it, and updates your UI automatically.
+
+
+// Why Axios is popular
+
+// Fetch requires manual JSON parsing.
+// Errors need manual handling.
+// Axios is shorter and easier to use, especially in real projects.
