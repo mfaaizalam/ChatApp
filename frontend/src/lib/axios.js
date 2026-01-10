a9702a7f61c1cb3a69e9f0835ae3eebf7fcd1513
mfaaizalam <mfaaiz2000bce@gmail.com>
2026-01-11 04:39:22 +0500

    fix axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,13 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//     baseURL:"http://localhost:5001/api",
+//     withCredentials:true, //send cookies with requests
+// })
+
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:5001/api",
-    withCredentials:true, //send cookies with requests
+    baseURL: import.meta.env.VITE_API_URL, // use Vercel backend URL
+    withCredentials: true, // send cookies with requests
 })
