4fe097f8d490eae786df23f2045db4c3750340f9
mfaaizalam <mfaaiz2000bce@gmail.com>
2026-01-11 15:24:14 +0500

    correct api

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,13 +1,6 @@
-// import axios from "axios";
-
-// export const axiosInstance = axios.create({
-//     baseURL:"http://localhost:5001/api",
-//     withCredentials:true, //send cookies with requests
-// })
-
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL, // backend URL
-  withCredentials: true,
-});
+    baseURL:"http://localhost:5001/api",
+    withCredentials:true, //send cookies with requests
+})
