5f5ee60c2acaa5dfb166f47ec0cf15cf58796147
mfaaizalam <mfaaiz2000bce@gmail.com>
2026-02-08 02:01:39 +0500

    deply

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"https://chat-app-phi-silk-48.vercel.app",
-        // baseURL:"http://localhost:5001/api",
+    // baseURL:"https://chat-app-phi-silk-48.vercel.app/api",
+        baseURL:"http://localhost:5001/api",
     withCredentials:true, //send cookies with requests
 })
 
