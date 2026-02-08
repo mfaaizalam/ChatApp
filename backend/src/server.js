357649eaa78db6d1336d5f3e9acff7ec52593e56
mfaaizalam <mfaaiz2000bce@gmail.com>
2026-02-08 13:28:03 +0500

    again

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,8 +17,9 @@ app.use(cors({
 }))
 
 
+connectDB();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));``
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/auth",authRoutes)
 app.use("/api/user",userRoutes)
@@ -26,10 +27,6 @@ app.use("/api/chat",chatRoutes)
 app.use("/chat", chatRoutes);
 app.use("/chat",aichat );
 app.get("/", (req, res) => res.send("Backend is running!"));
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on PORT ${PORT}`);
-    connectDB();
-})
-
+export default app;
